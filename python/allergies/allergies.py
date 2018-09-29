08f706b3f15e8bd131abd65f690c8f426194212f
Anirudha Pimpale <pimpale.anirudha94@gmail.com>
2018-09-29 14:07:19 -0400

    allergies

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,39 +1,39 @@
 import math
 class Allergies(object):
 
-    def nearest_power_of_2(self, value):
-        return 2 ** int(math.log(value,2))
-
     def get_allergant_name(self, value):
-        if value == 1: return 'eggs'
-        elif value == 2: return 'peanuts'
-        elif value == 4: return 'shellfish'
-        elif value == 8: return 'strawberries'
-        elif value == 16: return 'tomatoes'
-        elif value == 32: return 'chocolate'
-        elif value == 64: return 'pollen'
-        elif value == 128: return 'cats'
+        if value == 0: return 'eggs'
+        elif value == 1: return 'peanuts'
+        elif value == 2: return 'shellfish'
+        elif value == 3: return 'strawberries'
+        elif value == 4: return 'tomatoes'
+        elif value == 5: return 'chocolate'
+        elif value == 6: return 'pollen'
+        elif value == 7: return 'cats'
 
-    def make_list(self, score):
-        allergant_list = []
-        while score > 1:
-            nearest_score = self.nearest_power_of_2(score)
-            allergant_list.append(self.get_allergant_name(nearest_score))
-            score = score - nearest_score
-        if score == 1:
-            allergant_list.append('eggs')
-        return list(filter(None.__ne__, allergant_list))
+    def make_allergant_list(self, index_list):
+        for index,item in enumerate(index_list):
+            index_list[index] = self.get_allergant_name(item)
+        return index_list
+
+    def find_allergant(self, score):
+        score = score % 256
+        allergant_list_index = []
+        index = 7
+        while score > 0:
+            remainder = score % (1<<index)
+            if score != remainder:
+                allergant_list_index.append(index)
+                score = remainder
+            index -= 1
+        return self.make_allergant_list(allergant_list_index)
     
     def __init__(self, score):
-        self.allergant_list = self.make_list(score)
+        self.allergant_list = self.find_allergant(score)
 
     def is_allergic_to(self, item):
-        if item in self.allergant_list:
-            return True
-        else:
-            return False
+        return item in self.allergant_list
 
     @property
     def lst(self):
         return self.allergant_list
-
