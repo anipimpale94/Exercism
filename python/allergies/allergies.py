28b1fa11c580a57e979d2f1d0f7852a164597138
Anirudha Pimpale <pimpale.anirudha1994@gmail.com>
2018-10-01 11:20:45 -0400

    allergies fixed

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,32 +1,26 @@
 import math
 class Allergies(object):
-
-    def get_allergant_name(self, value):
-        if value == 0: return 'eggs'
-        elif value == 1: return 'peanuts'
-        elif value == 2: return 'shellfish'
-        elif value == 3: return 'strawberries'
-        elif value == 4: return 'tomatoes'
-        elif value == 5: return 'chocolate'
-        elif value == 6: return 'pollen'
-        elif value == 7: return 'cats'
-
-    def make_allergant_list(self, index_list):
-        for index,item in enumerate(index_list):
-            index_list[index] = self.get_allergant_name(item)
-        return index_list
+    allergant = {
+        0: 'eggs',
+        1: 'peanuts',
+        2: 'shellfish',
+        3: 'strawberries',
+        4: 'tomatoes', 
+        5: 'chocolate',
+        6: 'pollen',
+        7: 'cats',
+    }
 
     def find_allergant(self, score):
         score = score % 256
         allergant_list_index = []
         index = 7
         while score > 0:
-            remainder = score % (1<<index)
-            if score != remainder:
-                allergant_list_index.append(index)
-                score = remainder
+            if score & (1<<index):
+                allergant_list_index.append(self.allergant[index])
+                score -= 1<<index
             index -= 1
-        return self.make_allergant_list(allergant_list_index)
+        return allergant_list_index
     
     def __init__(self, score):
         self.allergant_list = self.find_allergant(score)
