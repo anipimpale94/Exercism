da283a9fdf528ec18c07f5c491c8a9a411ac6b84
Anirudha Pimpale <pimpale.anirudha1994@gmail.com>
2018-10-03 09:21:12 -0400

    allergies

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,25 +1,24 @@
 import math
 class Allergies(object):
     allergant = {
-        0: 'eggs',
-        1: 'peanuts',
-        2: 'shellfish',
-        3: 'strawberries',
-        4: 'tomatoes', 
-        5: 'chocolate',
-        6: 'pollen',
-        7: 'cats',
+        1:'eggs', 2:'peanuts', 4:'shellfish', 8:'strawberries', 16:'tomatoes', 32:'chocolate', 64:'pollen', 128:'cats',
+    }
+    allergant_rev = {
+        'eggs':1, 'peanuts':2 , 'shellfish':4, 'strawberries':8, 'tomatoes':16, 'chocolate':32, 'pollen':64, 'cats':128,
     }
 
     def find_allergant(self, score):
-        return [index for index in range(7,-1,-1) if score & (1<<index)]
+        return [1<<index for index in range(7,-1,-1) if score & (1<<index)]
     
     def __init__(self, score):
-        self.allergant_list_index = self.find_allergant(score % 256)
+        self.score = score % 256
+        self.allergant_list = self.find_allergant(self.score)
 
     def is_allergic_to(self, item):
-        return [key for key, value in self.allergant.items() if value == item][0] in self.allergant_list_index
+        # return [key for key, value in self.allergant.items() if value == item][0] in self.allergant_list
+        return bool(self.allergant_rev[item] & self.score)
+
 
     @property
     def lst(self):
-        return [self.allergant[item] for item in self.allergant_list_index]
+        return [self.allergant[item] for item in self.allergant_list]
