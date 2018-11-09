eec41012ebbe861fd524decdf12ad5f5b92ed1ae
anipimpale94 <pimpale1994@gmail.com>
2018-11-09 21:21:44 +0000

    grains

---
+++ bash/grains/grains.sh
@@ -0,0 +1,21 @@
+#!/bssin/bash
+main() {
+ if [ -z "$1" ]; then
+  echo Error: invalid input
+  exit 1
+ else
+  if [ "$1" -gt 0 ] && [ "$1" -lt 65 ]; then
+   echo $(( 1<< $(( $1 - 1 )) ))
+   exit 0
+  else
+   if [ "$1" == "total" ]; then
+    echo $(( $(( 1<<$1  )) - 1 ))
+    exit 0
+   fi
+   echo Error: invalid input
+   exit 1
+  fi
+ fi
+}
+
+main "$@"
