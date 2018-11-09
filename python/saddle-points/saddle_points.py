9fca411fa3b0ecaa320d7a9e612816b3b0832804
Anirudha Pimpale <pimpale.anirudha1994@gmail.com>
2018-11-09 13:59:35 -0500

    saddle points

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,27 @@
+import numpy as np
 def saddle_points(matrix):
-    pass
+    np_matrix = np.array(matrix)
+    if len(np_matrix.shape) == 1:
+        if matrix == []:
+            return set()
+        raise ValueError("Irregular Matrix")
+    saddle_points = []
+    for row_index in range(0, np_matrix.shape[0]):
+        min_row = np_matrix[row_index][0]
+        stack = [0]
+        for column_index in range(1, np_matrix.shape[1]):
+            if min_row < np_matrix[row_index][column_index]:
+                stack = [column_index]
+                min_row = np_matrix[row_index][column_index]
+            elif min_row == np_matrix[row_index][column_index]:
+                stack.append(column_index)
+
+        for item in stack:
+            flag = 1
+            for row_index_saddle in range(0, np_matrix.shape[0]):
+                if np_matrix[row_index][item] > np_matrix[row_index_saddle][item]:
+                    flag = 0
+                    break;
+            if flag:
+                saddle_points.append((row_index, item))               
+    return set(saddle_points)
