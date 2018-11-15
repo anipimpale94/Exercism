bfacb551c2928162194a1eb90da72b8f7385d9eb
anipimpale94 <pimpale1994@gmail.com>
2018-11-15 15:56:50 +0000

    rna

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -2,9 +2,10 @@ export const toRna = (dnaStrand) => {
  let dna = "";
  for (let letter of dnaStrand) {
   if (letter === 'G') dna = dna + 'C';
-  if (letter === 'C') dna = dna + 'G';
-  if (letter === 'T') dna = dna + 'A';
-  if (letter === 'A') dna = dna + 'U';
+  else if (letter === 'C') dna = dna + 'G';
+  else if (letter === 'T') dna = dna + 'A';
+  else if (letter === 'A') dna = dna + 'U';
+  else throw "Invalid input DNA.";
  }
  return dna;
 }
